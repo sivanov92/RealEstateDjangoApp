1f47634baf7cc3db85dba1021299d38f76d020fb
Stanislav.Ivanov <stanislav_ivanov9213@abv.bg>
2020-08-24 11:25:56 +0300

    GET endpoint for the agents, also updated models !

--- rproject/reapp1/models.py
+++ rproject/reapp1/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class Agents(models.Model):
+    agent_name = models.CharField(max_length=100,primary_key=True)
+    agent_adress = models.CharField(max_length=200)
+    agent_contact_phone = models.CharField(max_length=100)    
+    agents = models.Manager()
+
 class Estates(models.Model):
     estate_id = models.AutoField(primary_key=True)
     estate_title = models.CharField(max_length=100)
@@ -9,9 +15,6 @@ class Estates(models.Model):
     estate_construction_type = models.CharField(max_length=100)
     estate_status = models.CharField(max_length=20)
     estate_completion_year = models.CharField(max_length=4)
-
-class Agents(models.Model):
-    agent_id = models.AutoField(primary_key=True)
-    agent_name = models.CharField(max_length=100)
-    agent_adress = models.CharField(max_length=200)
-    agent_contact_phone = models.CharField(max_length=100)    
+    estate_agent = models.ForeignKey(Agents,on_delete=models.CASCADE)
+    estates=models.Manager()
+    
--- rproject/reapp1/views.py
+++ rproject/reapp1/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+# Imports for the serialization
+from django.core import serializers 
+from models import Agents,Estates
+from django.http import JsonResponse
 # Create your views here.
+def agents(request):
+    if request.method == "GET":
+        jsonserializer = serializers.get_serializer("json")
+        json_serializer = jsonserializer
+        json_serializer.serialize(Agents.agents.all())
+        data=json_serializer.getvalue()
+        return JsonResponse(data,safe=True)
+    elif request.method =="POST":
+        return JsonResponse({'message':'RESPONSE'})   
